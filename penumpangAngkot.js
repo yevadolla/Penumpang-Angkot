d580defb9f5d3672f5e2e98a2fa0ffc062d134d3
yevadolla <yevadolla@gmail.com>
2022-09-25 23:57:49 +0700

    object penumpang

--- penumpangAngkot.js
+++ penumpangAngkot.js
@@ -36,3 +36,32 @@ var hapusPenumpang = function(nm, penumpang){
         }
     }
 }
+
+//membuat object angkot
+function Angkot (sopir, trayek, penumpang, kas){
+    this.sopir = sopir;
+    this.trayek = trayek;
+    this.penumpang = penumpang;
+    this.kas = kas;
+
+    this.penumpangNaik = function(namaPn){
+        this.penumpang.push(namaPn);
+        return this.penumpang;
+    }
+    this.penumpangTurun = function(namaPn, bayar){
+        if(this.penumpang.length == 0){
+            alert('angkot masih kosong');
+            return false;
+        }
+        for (var i=0; i < this.penumpang.length; i++){
+            if(this.penumpang[i]==namaPn){
+                this.penumpang[i]=undefined;
+                this.kas+=bayar;
+                return this.penumpang;
+            }
+        }
+    }
+}
+
+var angkot1 = new Angkot('Adnan',['Cicaheum', 'Cipabiru'], [], 0 );
+var angkot2 = new Angkot('Adam', ['Pasar Cina', 'Baning'], [], 0);
